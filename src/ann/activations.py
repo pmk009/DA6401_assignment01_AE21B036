eddffd77dde0bbcac3ada99f01b2855c7c03ffaf
Kishore <mk009.mrj@gmail.com>
2026-02-20 22:58:40 +0530

    activations.py: created a base class for Activation functions, assuming no trainable parameters and Sigmoid activation class is created.
    neural_layer.py: created the neural layer class. keeping the modularity in mind.
    
    Note: the convention of error signal is changed for efficient implementation.

--- src/ann/activations.py
+++ src/ann/activations.py
@@ -2,3 +2,30 @@
 Activation Functions and Their Derivatives
 Implements: ReLU, Sigmoid, Tanh, Softmax
 """
+
+import numpy as np
+
+
+# Base class to represent activations
+class Activation:
+
+    def __init__(self):
+        pass
+
+    def forward(self, ak):
+        pass
+
+    def gradient(self, ak):
+        pass
+
+
+class Sigmoid(Activation):
+
+    def forward(self, ak):
+
+        return 1/(1+np.exp(-ak))
+    
+    def gradient(self, ak):
+
+        y = self.forward(ak)
+        return y(1-y) 
--- src/ann/neural_layer.py
+++ src/ann/neural_layer.py
@@ -2,3 +2,47 @@
 Neural Layer Implementation
 Handles weight initialization, forward pass, and gradient computation
 """
+
+import numpy as np
+from activations import *
+
+
+class neural_layer:
+
+    def __init__(self, nk: int=128, nk_1: int=128, activation: Activation=Sigmoid, initialization: str='random'):
+
+        self.num_neurons = nk
+        self.prev_num_neurons = nk_1 
+        self.activation = activation()
+
+        self.hk = None
+        self.hk_1 = None
+        self.ak = np.zeros(shape=(nk,))
+
+        self.grad_W = np.zeros(shape=(nk_1,nk))
+        self.grad_b = np.zeros(shape=(nk,))
+
+        if initialization not in ['random','xavier']:
+            raise ValueError('initialization should be random or xavier')
+        
+        elif initialization == 'random':
+
+            self.W = np.random.random(size=(nk_1,nk))
+            self.b = np.random.random(size=(nk,))
+        else:
+            pass
+
+    def forward(self, hk_1):
+
+        self.hk_1 = hk_1
+
+        np.matmul(self.W.T,hk_1,out=self.ak)
+        ak+= self.b
+
+        self.hk = self.activation.forward(ak)
+    
+
+    def backward(self, del_k):
+
+        np.multiply(del_k, self.activation.gradient(self.ak), out=self.grad_b)
+        np.outer(self.hk_1, self.grad_b, out=self.grad_W)
